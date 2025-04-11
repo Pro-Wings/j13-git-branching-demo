03722fa616a336f785be205d51e19dddb6c87f67
Pro-Wings <81178769+Pro-Wings@users.noreply.github.com>
2025-04-11 10:32:14 +0530

    Dev2 Updated HelloWorld.java

--- src/main/java/com/prowings/sample/HelloWorld.java
+++ src/main/java/com/prowings/sample/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello Java developers.. welcome to Git Branching!!!");
+		System.out.println("Hello Java developers.. welcome to Git Branching222222222!!!");
 	}
 
 }
