48f681ce5290ad1a217705a6de4ac80155d11a31
JD <jayprowings@gmail.com>
2025-04-11 10:30:43 +0530

    first commit

---
+++ src/main/java/com/prowings/sample/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.prowings.sample;
+
+public class HelloWorld {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("Hello Java developers.. welcome to Git Branching!!!");
+	}
+
+}
