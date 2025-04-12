34b9ae4154b07e62c361486685e551dbfc953b53
JD <jayprowings@gmail.com>
2025-04-12 10:47:54 +0530

    reduced code

--- src/main/java/com/prowings/sample/HelloWorld.java
+++ src/main/java/com/prowings/sample/HelloWorld.java
@@ -28,8 +28,6 @@ public class HelloWorld {
 	private static int substraction() {
 		int a = 10;
 		int b = 5;
-		int res = a - b;
-		System.out.println("Substraction is :" + res);
-		return res;
+		return a - b;
 	}
 }
